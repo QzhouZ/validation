f57c20e59ea6d467c0ff83adb50cb2e5db366eed
qzhou <448482356@qq.com>
2015-04-16 17:22:45 +0800

    add length valid

--- validation.js
+++ validation.js
@@ -6,8 +6,7 @@
  */
 define(function (require, exports, module) {
     'use strict';
-    var popup = require("custom/popup");
-                require("form");
+    require("lib/jquery.form");
     var validatebox = function(options) {
         var _default = {
             formId: "#form",
